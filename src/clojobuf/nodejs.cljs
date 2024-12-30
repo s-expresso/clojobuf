2293212072c534007d1d20929afbc0df3ee81e9c
st.cheewah <103625673+st-cheewah@users.noreply.github.com>
2024-12-30 23:20:26 +0800

    nodejs: fix protoc missing call to vschemas-update-msg-field-presence

--- src/clojobuf/nodejs.cljs
+++ src/clojobuf/nodejs.cljs
@@ -1,6 +1,6 @@
 (ns clojobuf.nodejs
   (:require [clojobuf.core :refer [->malli-registry]]
-            [clojobuf.schema :refer [xform-ast]]
+            [clojobuf.schema :refer [xform-ast vschemas-update-msg-field-presence]]
             [rubberbuf.core :as rc] ; rubberbuf.core uses rubberbuf.util which uses cljs-node-io.core that is not available to cljs browser runtime
             [rubberbuf.ast-postprocess :refer [unnest]]
             ))
@@ -13,6 +13,7 @@
         codec_malli_pairs (transduce (map (comp xform-ast val)) into [] rast)
         codec             (transduce (map first)                into {} codec_malli_pairs)
         malli             (transduce (map second)               into {} codec_malli_pairs)
+        malli             (vschemas-update-msg-field-presence malli)
         malli (if auto-malli-registry
                 (->malli-registry malli)
                 malli)]
